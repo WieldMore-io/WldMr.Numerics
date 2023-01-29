41af6fe133aff40e14e19292e5ab31f54ab37198
PierreYvesR <py.rivaille@gmail.com>
2023-01-29 13:30:22 +0000

    Mat.solveMV, Mat.ofRowsArray, Mat.ofColsArray Vec.addVV, Vec.SubVV, Vec,absMax

---
+++ src/WldMr.Numerics.LinAlg/Vec.fs
@@ -0,0 +1,18 @@
+﻿namespace WldMr.Numerics.LinAlg
+
+
+module Vec =
+  let addVV (a: float[]) (b: float[]) =
+    if a.Length <> b.Length then failwith $"Arrays of different size {a.Length} {b.Length}"
+    Array.init a.Length (fun i -> a.[i] + b.[i])
+
+  let subVV (a: float[]) (b: float[]) =
+    if a.Length <> b.Length then failwith $"Arrays of different size {a.Length} {b.Length}"
+    Array.init a.Length (fun i -> a.[i] - b.[i])
+
+  let absMax (v: float[]) =
+    let mutable r = 0.
+    for x in v do
+      let a = x |> abs
+      if a > r then r <- a
+    r
