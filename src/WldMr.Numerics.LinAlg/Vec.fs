ab9bcffdbd2f1f9aeb1c9159e81de403312a3549
PierreYvesR <py.rivaille@gmail.com>
2023-03-24 18:12:02 +0000

    Vec: dotProduct, mulVV, mulSV

--- src/WldMr.Numerics.LinAlg/Vec.fs
+++ src/WldMr.Numerics.LinAlg/Vec.fs
@@ -16,3 +16,30 @@ module Vec =
       let a = x |> abs
       if a > r then r <- a
     r
+
+  let inline dotProduct (x: float[]) (y: float[]) =
+    let xl = x.Length
+    let yl = y.Length
+    if (xl = 0) || (yl = 0) then
+        0.
+    elif xl <> yl then
+        failwith $"Arrays of different size {xl} {yl}"
+    else
+        Blas.ddot(xl, x, 1, y, 1)
+
+  let inline mulVV (x: float[]) (y: float[]) =
+    let xl = x.Length
+    let yl = y.Length
+    if (xl = 0) || (yl = 0) then
+        0.
+    elif xl <> yl then
+        failwith $"Arrays of different size {xl} {yl}"
+    else
+        Blas.ddot(xl, x, 1, y, 1)
+
+  let inline mulSV (x: float) (y: float[]) =
+    let yl = y.Length
+    if (yl = 0) then
+        [||]
+    else
+      Array.init yl (fun i -> x * y.[i])
