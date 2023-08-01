cffdb9441c071b6ce8aa6de28ecae12d22a7d17b
Teerawat Wuttiwat <tw@wieldmore.com>
2023-08-01 17:52:03 +0700

    add infinity functions for Python

--- src/WldMr.Numerics.LinAlg/FableExt.fs
+++ src/WldMr.Numerics.LinAlg/FableExt.fs
@@ -6,23 +6,41 @@ module System =
   module Double =
 
 
+    #if FABLE_COMPILER_JAVASCRIPT
     [<Emit("$0 === Number.NEGATIVE_INFINITY")>]
-    let private isNegativeInfinity (x:double): bool = jsNative
+    let private isNegativeInfinityJS (x:double): bool = jsNative
 
     [<Emit("$0 === Number.POSITIVE_INFINITY")>]
-    let private isPositiveInfinity (x:double): bool = jsNative
+    let private isPositiveInfinityJS (x:double): bool = jsNative
+    #endif
+
+    #if FABLE_COMPILER_PYTHON
+    [<Import("inf", "math")>]
+    let inf : float = nativeOnly
+
+    let isNegativeInfinityPy (x:double): bool = (x = (-inf))
+
+    let isPositiveInfinityPy (x:double): bool = (x = inf)
+    #endif
 
     let IsPositiveInfinity x =
       #if !FABLE_COMPILER
-        global.System.Double.IsPositiveInfinity x
+      global.System.Double.IsPositiveInfinity x
       #else
-        isPositiveInfinity x
+        #if FABLE_COMPILER_PYTHON
+        isPositiveInfinityPy x
+        #else
+        isPositiveInfinityJS x
+        #endif
       #endif
 
     let IsNegativeInfinity x =
       #if !FABLE_COMPILER
-        global.System.Double.IsNegativeInfinity x
+      global.System.Double.IsNegativeInfinity x
       #else
-        isNegativeInfinity x
+        #if FABLE_COMPILER_PYTHON
+        isNegativeInfinityPy x
+        #else
+        isNegativeInfinityJS x
+        #endif
       #endif
-
